218848541f02f373608b0fc8003469f4eaafe427
MandoARR <armando_arr@outlook.com>
2024-05-25 00:36:45 -0600

    Respaldo

--- Escuela_CFNC/src/components/Form_alumnos.tsx
+++ Escuela_CFNC/src/components/Form_alumnos.tsx
@@ -1,7 +1,20 @@
+import { Button, Card, CardContent, Input, Stack } from "@mui/material";
+
 export function Form_alumnos() {
-    return (
-        <>
-          <div>Formulario</div> 
-        </>// Revisar viejos apuntes del formulario de la materia Hypermedia
-    )
+  return (
+    <>
+      <br></br>
+      <form>
+        <Card sx={{ maxWidth: 450 }}>
+          <CardContent>
+            <Stack spacing={1}>
+              <Input placeholder="Nombre" name="nombre"></Input>
+              <Input placeholder="Apellido" name="apellido"></Input>
+              <Button type="submit">Agregar</Button>
+            </Stack>
+          </CardContent>
+        </Card>
+      </form>
+    </>
+  )
 }
--- Escuela_CFNC/src/components/Pruebas.tsx
+++ Escuela_CFNC/src/components/Pruebas.tsx
@@ -1,19 +1,29 @@
 import { useState } from "react"
 import { Form_alumnos } from "./Form_alumnos"
+import Barra from "./Barra"
 
 export function Pruebas() {
-    const [banner, setBanner] = useState(Boolean)
-    
-    const handleForm = () => {
+    const [banner, setBanner] = useState<boolean>(true)
+
+    const handleNew = () => {
         setBanner(true)
+    }
+
+    const handleCancel = () => {
+        setBanner(false)
     }
 
     return (
         <>
-
+            <Barra />
             <h1>Componente de Pruebas</h1>
-            <button onClick={handleForm}>Nuevo</button>
-            {banner === true ? <div><Form_alumnos/></div> :''}
+            <section>
+                {banner === true ? <div><Form_alumnos /></div> : ''}
+                <br></br>
+                <button onClick={handleNew}>Nuevo</button>
+                <button onClick={handleCancel}>Cancelar</button>
+            </section>
+
         </>
     )
 }
