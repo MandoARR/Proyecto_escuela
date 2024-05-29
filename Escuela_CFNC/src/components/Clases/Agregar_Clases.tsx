3a0a54ced08123801661456e2af17cfa808a91fb
MandoARR <150207139+MandoARR@users.noreply.github.com>
2024-05-29 16:20:14 -0600

    Create Agregar_Clases.tsx

---
+++ Escuela_CFNC/src/components/Clases/Agregar_Clases.tsx
@@ -0,0 +1,46 @@
+import { CircularProgress } from "@mui/material";
+import { useState } from "react";
+import { postStudents } from "../../services/studets-services";
+import { redirect, useNavigate } from "react-router-dom";
+import { Form_agregar } from "./Form_agregar";
+
+
+export function Agregar_Clases() {
+
+    const [loading, setLoading] = useState<Boolean>(false)
+
+
+    const navigate = useNavigate()
+
+
+    const agregarAlumnos = (body: any) => {
+        postStudents(body)
+    }
+
+    const handleAgregarDB = (e: any) => {
+
+        e.preventDefault();
+
+        setLoading(true)
+
+        const formData = new FormData(e.currentTarget);
+        const formJson = Object.fromEntries((formData as any).entries());
+        formJson.uuid = crypto.randomUUID()
+        agregarAlumnos(formJson)
+        setLoading(false)
+
+        //navigate('/admin/pagina_de_carga') Preguntar como mandar una pagina con tiempo para fingi que esta cargando
+        navigate('/admin')
+        // cambiar el componente para fingir que esta cargando
+    }
+
+    return (
+        <>
+            <br></br>
+            <form onSubmit={handleAgregarDB}>
+                <Form_agregar />
+            </form>
+            {loading && <CircularProgress />}
+        </>
+    )
+}
