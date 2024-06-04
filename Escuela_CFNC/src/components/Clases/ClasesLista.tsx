a56b6fefee956400742ccae0b61a4fd8fa5c7d5c
MandoARR <150207139+MandoARR@users.noreply.github.com>
2024-06-04 16:31:39 -0600

    Update

--- Escuela_CFNC/src/components/Clases/ClasesLista.tsx
+++ Escuela_CFNC/src/components/Clases/ClasesLista.tsx
@@ -1,31 +1,33 @@
 import { useState, useEffect } from 'react';
 import { IClases } from '../../store/IClases';
 import { deleteClases } from '../../services/clases-services';
+import { FormAgregar } from './FormAgregar';
 
 const url = "https://backend-subs-control.onrender.com/api/clase";
 
 export function ClasesLista() {
     const [clases, setClases] = useState<IClases[]>([]);
     const [selected, setSelected] = useState<IClases | null>(null)
-
+    const [banner, setBanner] = useState<Boolean>(false)
     useEffect(() => {
         fetch(url)
             .then(response => response.json())
             .then(data => {
-                setClases(data); 
+                setClases(data);
             })
     }, []);
 
-    const handleDelete = (id:string) => {
+    const handleDelete = (id: string) => {
         deleteClases(id)
-        .then(()=> {
-            setClases(clases.filter((clase) => clase.id !== id))
-        })
+            .then(() => {
+                setClases(clases.filter((clase) => clase.id !== id))
+            })
     }
 
     const handleUpdate = (clase: IClases) => {
         setSelected(clase)
         console.log(clase)
+        setBanner(true)
     }
 
     /*  useEffect(() => {
@@ -33,27 +35,35 @@ export function ClasesLista() {
     }, []);*/
 
     return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>NOMBRE</th>
-                        <th>COSTO</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {clases.map((clase) => (
-                        <tr key={clase.id}>
-                            <td>{clase.id}</td>
-                            <td>{clase.nombre}</td>
-                            <td>${clase.costo}</td>
-                            <td><button onClick={() => handleDelete(clase.id)}> X </button></td>
-                            <td><button onClick={() => handleUpdate(clase)}> Actualizar </button></td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+        <>
+            <section>
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>NOMBRE</th>
+                                <th>COSTO</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {clases.map((clase) => (
+                                <tr key={clase.id}>
+                                    <td>{clase.id}</td>
+                                    <td>{clase.nombre}</td>
+                                    <td>${clase.costo}</td>
+                                    <td><button onClick={() => handleDelete(clase.id)}> X </button></td>
+                                    <td><button onClick={() => handleUpdate(clase)}> Actualizar </button></td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+
+            <section>
+                {banner != true ? null : <FormAgregar/>}
+            </section>
+        </>
     );
 }
