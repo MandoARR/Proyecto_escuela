6d203b09437321f49051479e4501c1306a20411f
MandoARR <150207139+MandoARR@users.noreply.github.com>
2024-06-13 15:31:39 -0600

    Suscripciones

---
+++ Escuela_CFNC/src/components/Suscripciones/SuscripcionesLista.tsx
@@ -0,0 +1,7 @@
+export function SuscripcionesLista() {
+    return (
+        <>
+            <h1>Suscripciones que saben a Inscripciones</h1>
+        </>
+    )
+}
--- Escuela_CFNC/src/sections/Suscripciones_interfaz.tsx
+++ Escuela_CFNC/src/sections/Suscripciones_interfaz.tsx
@@ -1,7 +1,9 @@
+import { SuscripcionesLista } from "../components/Suscripciones/SuscripcionesLista";
+
 export function Suscripciones_interfaz() {
     return (
         <>
-            <h1>Suscripciones que saben a Inscripciones</h1>
+            <SuscripcionesLista />
         </>
     )
 }
