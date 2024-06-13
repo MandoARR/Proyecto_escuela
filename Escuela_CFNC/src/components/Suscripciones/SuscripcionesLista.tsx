cad81247f70da1f921b7d9d270d0e87823031455
MandoARR <150207139+MandoARR@users.noreply.github.com>
2024-06-13 16:47:09 -0600

    update

--- Escuela_CFNC/src/components/Suscripciones/SuscripcionesLista.tsx
+++ Escuela_CFNC/src/components/Suscripciones/SuscripcionesLista.tsx
@@ -1,7 +1,47 @@
+import { useState } from "react"
+import { ISuscripciones } from "../../store/ISuscripciones"
+import { getSuscriptions } from "../../services/suscriptions-services"
+import { Sheet, Table } from "@mui/joy"
+
 export function SuscripcionesLista() {
+    const [suscripciones, setSuscripciones] = useState<ISuscripciones[]>([])
+
+    getSuscriptions()
+        .then(data => {
+            setSuscripciones(data)
+        })
+
     return (
         <>
             <h1>Suscripciones que saben a Inscripciones</h1>
+            <div>
+                <Sheet>
+                    <Table aria-label="striped table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>ALUMNO</th>
+                                <th>CLASE</th>
+                                <th>COSTO</th>
+                                <th>DIA DE PAGO</th>
+                                <th>ESTADO</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {suscripciones.map((suscripcion) => (
+                                <tr key={suscripcion.id}>
+                                    <td>{suscripcion.alumno}</td>
+                                    <td>{suscripcion.clase}</td>
+                                    <td>{suscripcion.costo}</td>
+                                    <td>{suscripcion.diaPago}</td>
+                                    <td>{suscripcion.estado}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                </Sheet>
+
+            </div>
         </>
     )
 }
---
+++ Escuela_CFNC/src/store/ISuscripciones.ts
@@ -0,0 +1,8 @@
+export interface ISuscripciones{
+    id:string,
+    alumno: string,
+    clase:string,
+    costo:number,
+    diaPago:number, // preguntar en clase cual especificacion recomiendan
+    estado:number
+}
